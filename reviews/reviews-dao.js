0cdfdc7f6b5365bf41325fdc6a0a7d61960149b6
likuny0415 <li.kuny@husky.neu.edu>
2022-12-13 01:09:54 -0500

    review CRUD

--- reviews/reviews-dao.js
+++ reviews/reviews-dao.js
@@ -1,13 +1,29 @@
 import reviewsModel from "./reviews-model.js";
 
-export const createReview = (review) =>
-    reviewsModel.create(review)
+export const createReview = (review) => reviewsModel.create(review);
 
 export const findReviewsByMovie = (imdbID) =>
-    reviewsModel
-        .find({imdbID})
-        .populate('author')
-        .exec()
+  reviewsModel.find({ imdbID }).populate("author").exec();
 
-export const findReviewsByAuthor = (author) =>
-    reviewsModel.find({author})
+export const findReviewsByAuthor = (author) => reviewsModel.find({ author });
+
+export const findReviewsBySong = (songID) =>
+  reviewsModel.find({ songID: songID }).exec();
+
+export const deleteReview = async (reviewID) => {
+  const status = await reviewsModel.deleteOne({ _id: reviewID });
+  return status;
+};
+
+
+// export const updateReview = async (reviewID, reviewUpdates) => {
+//     const status = await reviewsModel.updateOne(
+//         {_id: reviewID},
+//         {$set: reviewUpdates}
+//     );
+//     return status;
+//   };
+
+
+export const updateReview = (tid, tuit) =>
+    reviewsModel.updateOne({ _id: tid }, { $set: tuit });
